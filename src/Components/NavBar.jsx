e5613c4b69679e27b6f2a130c4d7ee9a65550c55
Yordanos-zegeye <yordanoszegeye24@gmail.com>
2024-09-17 11:46:32 +0300

    added some basic styling for the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/products'>Products</Link>
-        <Link to='/gallery'>Gallery</Link>
-        <Link to='/contact'>Contact</Link>
+    <div className='flex gap-4 justify-end m-5 text-cyan-500 text-base'>
+        <Link to='/' className='hover:underline'>Home</Link>
+        <Link to='/about' className='hover:underline'>About</Link>
+        <Link to='/products' className='hover:underline'>Products</Link>
+        <Link to='/gallery' className='hover:underline'>Gallery</Link>
+        <Link to='/contact' className='hover:underline'>Contact</Link>
 
     </div>
   )
